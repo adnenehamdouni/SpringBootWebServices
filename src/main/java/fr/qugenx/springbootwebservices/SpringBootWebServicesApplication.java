fc743b2037f9cf5bc9c764ffa8ce8a6aa769848e
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-11-27 17:41:48 +0100

    first commit

---
+++ src/main/java/fr/qugenx/springbootwebservices/SpringBootWebServicesApplication.java
@@ -0,0 +1,13 @@
+package fr.qugenx.springbootwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/qugenx/springbootwebservices/SpringBootWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.qugenx.springbootwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
