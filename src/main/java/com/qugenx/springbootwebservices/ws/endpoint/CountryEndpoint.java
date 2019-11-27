7e2f83950bd9f8e1d2818f6768563b17b670ac3a
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-11-27 19:11:49 +0100

    remove first method classes and ressources

--- src/main/java/com/qugenx/springbootwebservices/ws/endpoint/CountryEndpoint.java
+++ src/main/java/com/qugenx/springbootwebservices/ws/endpoint/CountryEndpoint.java
@@ -1,32 +1,9 @@
 package com.qugenx.springbootwebservices.ws.endpoint;
 
-import com.qugenx.springbootwebservices.ws.CountryRepository;
-import com.qugenx.springbootwebservices.ws.gen.GetCountryRequest;
-import com.qugenx.springbootwebservices.ws.gen.GetCountryResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
-import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class CountryEndpoint {
 
-    private static final String NAMESPACE_URI = "http://www.qugenx.com/genws-app/ws/gen";
 
-    private CountryRepository countryRepository;
-
-    @Autowired
-    public CountryEndpoint(CountryRepository countryRepository) {
-        this.countryRepository = countryRepository;
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
-    @ResponsePayload
-    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
-        GetCountryResponse response = new GetCountryResponse();
-        response.setCountry(countryRepository.findCountry(request.getName()));
-
-        return response;
-    }
 }
